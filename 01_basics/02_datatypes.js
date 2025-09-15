63e1aad37eeee72f68c4683756ee701dcbfebf16
suvratsavran18 <suvratsavran@gmail.com>
2025-09-15 20:02:00 +0000

    continued..

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+// let name="ankitdwivwdi"
+// let age=18
+
+// let height;
+
+// console.table([name,age,heigh[);
+
+
+    const str = "Hello World";
+    const index = str.indexOf("World");
+    console.log(index); // Output: 6

11493fa5ef2fff33e3caf8089c3e21eaf7e4c4a1
suvratsavran18 <suvratsavran@gmail.com>
2025-09-15 16:42:12 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const team="india"
+let playerName="viratkohli"
+var jerseyNo="18"
+runs="9647"
+
+
+//   team="royalchallenegerbengaluru"
+playerName="chiku"
+jerseyNo="05"
+runs="10653"
+console.log(team)
+console.table([team,playerName,jerseyNo,runs])

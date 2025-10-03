f7bb861d6042b5049d3fec67e1acea3fe1afec75
suvratsavran18 <suvratsavran@gmail.com>
2025-10-03 12:46:52 +0000

    continued

--- 01_basics/04_operators.js
+++ 01_basics/04_operators.js
@@ -1,8 +1,18 @@
-console.log("1"+2);
-console.log(1+"2"+3);
-console.log(2+"1");
-console.log(1+"2"+"3");
-console.log(1+2+"3");
-console.log("1"+2+3);
+// console.log("1"+2);
+// console.log(1+"2"+3);
+// console.log(2+"1");
+// console.log(1+"2"+"3");
+// console.log(1+2+"3");
+// console.log("1"+2+3);
 
-
+let value = 33
+let negValue = -value
+console.log(negValue)
+ 
+// console.log(true)
+// console.log(+true)
+// console.log(null)
+// console.log(+null)
+// console.log(+"")
+// console.log(false)
+// console.log(+false)
